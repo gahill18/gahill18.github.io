be12275dd04e75f34b3fbe103966a1babdb73746
Garrett Hill <gahill2018@protonmail.com>
2022-05-13 19:52:53 -0400

    Now saving json info in files

--- code/optimal-reddit/src/main.rs
+++ code/optimal-reddit/src/main.rs
@@ -7,6 +7,8 @@ If you use this code, please credit Garrett Hill @ gahill18.github.io
 extern crate reqwest;
 
 use std::env;
+use std::io::Write;
+use std::fs::{File,remove_file};
 
 use reqwest::blocking::{Response,Client};
 use reqwest::{Error,StatusCode};
@@ -34,15 +36,19 @@ struct Child {
 #[derive(Debug, Serialize, Deserialize)]
 struct Post {
     id:    String,
+    title: String,
+    created_utc: f64,
     score: i32,
-    created_utc: f64
+    num_comments: i32,
+    total_awards_received: i32
+    
 }
 
 // Gets the top posts of all time for the given sub
 fn get_top_for_sub(client: &Client, sub: &str) -> Result<Response, Error> {
     // Format the request string
     let get_str =
-	format!("https://old.reddit.com/r/{}/top/.json?t=all&?sort=top&t=alltime", sub);
+	format!("https://old.reddit.com/r/{}/top/.json?t=all&?sort=top&t=month", sub);
 
     // Get the response from reddit
     let response = client.get(get_str)
@@ -59,17 +65,35 @@ fn get_top_for_sub(client: &Client, sub: &str) -> Result<Response, Error> {
 }
 
 fn main() ->  Result<(), Error> {
-    // Environment variables execution location, subreddit
+    // Environment variables execution location, subreddit, path to storefile
     let args: Vec<String> = env::args().collect();
-    assert_eq!(args.len(), 2);
+    assert_eq!(args.len(), 3);
 
     // Get the top listings for the given subreddit
     let client = Client::new();
     let sub    = &args[1];
     let res    = get_top_for_sub(&client, &sub)?;
 
+    // Parse the response for the appropriate nested fields
     let listings = res.json::<Listing>()?;
-    println!("{:?}",listings);
+    let posts = listings.data.children;
+
+    // Get the path to store results in from user
+    let path = &args[2];
+    let fp = std::path::Path::new(path);
+
+    // If fp already exists, delete it
+    if fp.exists() {
+	remove_file(fp);
+    }
+
+    let mut file = File::create(fp).expect("create failed");
+
+    // Write children to file
+    for post in posts {
+	let out = format!("{:?}",post);
+	file.write_all(out.as_bytes());
+    }
 
     // If you've made it this far, nothing left to do
     Ok(())
