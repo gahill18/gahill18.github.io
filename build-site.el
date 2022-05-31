1cb23b62bcb55ea3aa5a3ed7a9b3b980d7c8f042
Garrett Hill <gahill2018@protonmail.com>
2022-05-31 14:43:14 -0400

    fixing bugs from file restructure

--- build-site.el
+++ build-site.el
@@ -26,7 +26,7 @@
              :publishing-function 'org-html-publish-to-html
 	     :with-author nil           ;; Don't include author name
              :with-creator t            ;; Include Emacs and Org versions in footer
-             :with-toc t                ;; Include a table of contents
+             :with-toc f                ;; Include a table of contents
              :section-numbers nil       ;; Don't include section numbers
              :time-stamp-file nil)))    ;; Don't include time stamp in file
 
